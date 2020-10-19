38481f2883ac3be40d44d963b90bea808a67dea4
David Arenas Aguirre <daa@Soologic-Mac-mini.local>
2020-10-19 16:43:33 +0200

    Nuevos cambios

---
+++ ios/Solfium_instalador-tvOSTests/Solfium_instalador_tvOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  Solfium_instalador_tvOSTests.swift
+//  Solfium_instalador-tvOSTests
+//
+//  Created by David Arenas Aguirre on 19/10/2020.
+//
+
+import XCTest
+@testable import Solfium_instalador_tvOS
+
+class Solfium_instalador_tvOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
